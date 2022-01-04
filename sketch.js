be501ba00c9980b8957d569222a0db578dada151
dasanuska <92671275+dasanuska@users.noreply.github.com>
2022-01-04 11:42:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,10 +8,8 @@ var world;
 var radius = 70;
 
 function preload(){
-
-	dustbinImg = addImage("dustbin.png")
-
-
+	dustbinImg = loadImage("dustbin.png");
+	paperImg = loadImage("paper.png");
 }
 
 
@@ -25,16 +23,17 @@ function setup() {
 	var ball_options={
 		isStatic:false,
 		restitution:0.3,
-		density:0.4
+		friction: 0,
+		density:1.2
 	}
 
-	ball = Bodies.circle(260,100,radius/2.6,ball_options);
+	ball = Bodies.circle(310,100,radius/2.6,ball_options);
 	World.add(world,ball);
 
 	ground=new Ground(width/2,670,width,20);
 	leftSide = new Ground(1100,600,10,120);
 	rightSide = new Ground(1270,600,10,120);
-	bottomSide = new Ground(1185,650,150,20);
+	bottomSide = new Ground(1190,650,150,20);
 
 	Engine.run(engine);
   
@@ -53,20 +52,20 @@ function draw() {
 
 	
 	imageMode(CENTER);
+	// use image() command to add paper image to the ball.
+	image(paperImg,ball.position.x,ball.position.y,radius,radius)
 
-image(ball.position.x,ball.position.y,radius,radius);
+	// use image() command to add dustbin image in the canvas.
+	image(dustbinImg, 1185, 570, 200,200)
 
 
-	
-	image(1185, 570, 200,200);
-	
 
 }
 
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+		Matter.Body.applyForce(ball,ball.position,{x:130,y:-145});
     
   	}
 }
